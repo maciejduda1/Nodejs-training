c0d68c60faf1ab8f03d5891beb6f048116cfbce6
Maciej Duda <maciej.duda1@gazeta.pl>
2017-12-03 15:43:06 +0100

    Nie działa

--- modules/OSinfo.js
+++ modules/OSinfo.js
@@ -1,6 +1,6 @@
 ﻿var os = require('os');
 var colors = require('colors');
-var timerSetup = require('./TimeConfig');
+//var timerSetup = require('./TimeConfig');
 
 function getOSinfo(){
 	var type = os.type();
@@ -12,14 +12,14 @@ function getOSinfo(){
 	else if (type === 'Windows_NT') {
 		type = 'Windows';
 	}
-	console.log('System:', type);
-	console.log('Release:', release);
+	console.log('System:'.green, type);
+	console.log('Release:'.red, release);
 
 	var cpu = os.cpus()[0].model;
-	console.log(cpu.red);
+	console.log(colors.green(cpu));
 
-	var uptime = os.uptime();
-	console.log('Uptime: ~'.green, timerSetup.time(uptime));
+//	var uptime = os.uptime();
+//	console.log('Uptime: ~', timerSetup.time(uptime));
 
 	var userInfo = os.userInfo();
 	console.log('User name: ', userInfo.username);
