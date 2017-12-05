e28a6a30e3f7b1f19ddc2fcad8416c6716eb2716
Maciej Duda <maciej.duda1@gazeta.pl>
2017-12-05 14:57:09 +0100

    wysyłanie plików z servera

--- index.js
+++ index.js
@@ -4,6 +4,42 @@ var emitter = new EventEmitter();
 var fs = require('fs');
 var StatMode = require('stat-mode');
 var colors = require('colors');
+
+var http = require('http');
+var server = http.createServer();
+/*
+server.on('request', function(request, response){
+	response.setHeader("Content-Type", "text/html; charset=utf-8");
+	if (request.method === 'GET' && request.url === '/hello'){
+		response.write('<h1>Hello World!</h1>');	
+		response.end();
+	} else {
+		response.statusCode = 404
+		response.write('<h1>404: Zła Ścieżka!</h1>');
+		response.end();
+	}
+	
+})
+server.listen(9000);
+*/
+
+	
+
+server.on('request', function(request, response){
+	response.setHeader("Content-Type", "text/html; charset=utf-8");
+	if (request.method === 'GET' && request.url === '/'){
+	fs.readFile('./index.html', 'utf-8', function(err, data){
+		response.write(data);
+		response.end();
+	});	
+	} else {
+		response.statusCode = 404
+		response.write('<h1>404: Zła Ścieżka!</h1>');
+		response.end();
+	}
+});
+server.listen(9000);
+
 /*
 fs.stat('./cat.jpg', function(err, stats){
 	var statMode = new StatMode(stats);
@@ -23,6 +59,7 @@ fs.readFile('./text.txt', 'utf-8', function(err, data){
 	});
 });
 */
+/*
 fs.readdir('./modules', function(err, files){
 	if (err) throw err;
 	console.log('W folderze modules znajdują się pliki: ' + files);
@@ -36,7 +73,7 @@ fs.readdir('./modules', function(err, files){
 		});
 	});
 });
-
+*/
 
 emitter.on('beforeCommand', function(instruction){
 	console.log('You wrote: ' + instruction + ' trying to run command');
